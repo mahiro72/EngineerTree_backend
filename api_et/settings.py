87d9f665f0db408a7187b506c25f101d6e5d113e
mahiro <mahiro.yoshie@gmail.com>
2021-11-09 21:00:01 +0900

    secret add

--- api_et/settings.py
+++ api_et/settings.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_et.settings')
 
 application = get_asgi_application()
